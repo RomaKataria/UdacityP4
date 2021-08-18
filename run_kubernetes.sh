fbd76e58a98f6f4da9ffbf57dd1a8c39528c129c
ec2-user <k.roma873@gmail.com>
2021-08-18 10:06:54 +0000

    kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=130987/udacity_prj4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl -- run latest --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl -- get pods
 
 # Step 4:
 # Forward the container port to a host
+minikube kubectl -- port-forward latest 8000:80
 
